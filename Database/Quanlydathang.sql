89de92a87d2dcc6dd566ec7a568ae220399c5b6f
Nguyen Hoang Linh <linh072217@gmail.com>
2021-05-03 08:19:06 +0700

    upload database for web

---
+++ Database/Quanlydathang.sql
@@ -0,0 +1,128 @@
+/*==============================================================*/
+/* DBMS name :     Quanlydathang                                */
+/* Created by :  HoangLinhPlus			                        */
+/*==============================================================*/
+
+
+drop table if exists CHITIETDATHANG;
+
+drop table if exists DATHANG;
+
+drop table if exists DIACHIKH;
+
+drop table if exists HANGHOA;
+
+drop table if exists KHACHHANG;
+
+drop table if exists LOAIHANGHOA;
+
+drop table if exists NHANVIEN;
+
+/*==============================================================*/
+/* Table: CHITIETDATHANG                                        */
+/*==============================================================*/
+create table CHITIETDATHANG
+(
+   SODONDH              int not null,
+   MSHH                 int not null,
+   SOLUONG              int not null,
+   GIADATHANG           int not null,
+   GIAMGIA              float,
+   primary key (SODONDH, MSHH)
+);
+
+/*==============================================================*/
+/* Table: DATHANG                                               */
+/*==============================================================*/
+create table DATHANG
+(
+   SODONDH              int not null,
+   MSKH                 int not null,
+   MSNV                 int,
+   NGAYDH               date not null,
+   NGAYGH               date,
+   TRANGTHAI            varchar(20) not null,
+   primary key (SODONDH)
+);
+
+/*==============================================================*/
+/* Table: DIACHIKH                                              */
+/*==============================================================*/
+create table DIACHIKH
+(
+   MADC                 int not null,
+   DIACHI               varchar(150) not null,
+   MSKH                 int not null,
+   primary key (MADC)
+);
+
+/*==============================================================*/
+/* Table: HANGHOA                                               */
+/*==============================================================*/
+create table HANGHOA
+(
+   MSHH                 int not null,
+   TENHH                varchar(50) not null,
+   LOCATION             varchar(200) not null,
+   QUYCACH              varchar(50) not null,
+   GIA                  int not null,
+   SOLUONGHANG          int not null,
+   MALOAIHANG           char(3) not null,
+   GHICHU               varchar(200),
+   primary key (MSHH)
+);
+
+/*==============================================================*/
+/* Table: KHACHHANG                                             */
+/*==============================================================*/
+create table KHACHHANG
+(
+   MSKH                 int not null,
+   HOTENKH              varchar(50) not null,
+   TENCONGTY            varchar(50),
+   SODIENTHOAI          varchar(11) not null,
+   EMAIL                varchar(70),
+   primary key (MSKH)
+);
+
+/*==============================================================*/
+/* Table: LOAIHANGHOA                                           */
+/*==============================================================*/
+create table LOAIHANGHOA
+(
+   MALOAIHANG           char(3) not null,
+   TENLOAIHANG          varchar(50) not null,
+   primary key (MALOAIHANG)
+);
+
+/*==============================================================*/
+/* Table: NHANVIEN                                              */
+/*==============================================================*/
+create table NHANVIEN
+(
+   MSNV                 int not null,
+   HOTENNV              varchar(50) not null,
+   CHUCVU               varchar(30) not null,
+   DIACHI               varchar(200) not null,
+   SODIENTHOAI          varchar(11) not null,
+   primary key (MSNV)
+);
+
+alter table CHITIETDATHANG add constraint FK_CHITIETDATHANG_DATHANG foreign key (SODONDH)
+      references DATHANG (SODONDH) on delete restrict on update restrict;
+
+alter table CHITIETDATHANG add constraint FK_CHITIETDATHANG_HANGHOA foreign key (MSHH)
+      references HANGHOA (MSHH) on delete restrict on update restrict;
+
+alter table DATHANG add constraint FK_DATHANG_KHACHHANG foreign key (MSKH)
+      references KHACHHANG (MSKH) on delete restrict on update restrict;
+
+alter table DATHANG add constraint FK_DATHANG_NHANVIEN foreign key (MSNV)
+      references NHANVIEN (MSNV) on delete restrict on update restrict;
+
+alter table DIACHIKH add constraint FK_DIACHIKH_KHACHHANG foreign key (MSKH)
+      references KHACHHANG (MSKH) on delete restrict on update restrict;
+
+alter table HANGHOA add constraint FK_HOANGHOA_LOAIHANGHOA foreign key (MALOAIHANG)
+      references LOAIHANGHOA (MALOAIHANG) on delete restrict on update restrict;
+
