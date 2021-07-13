4c45b41edbe08931a1e3a58ca99127d0d08a58b9
Nguyen Hoang Linh <linh072217@gmail.com>
2021-07-14 05:59:12 +0700

    update account database for customer

--- php/ConnectDB.php
+++ php/ConnectDB.php
@@ -1,8 +1,8 @@
 <?php
   define('DB_HOST', 'localhost');
   define('DB_NAME', 'quanlydathang');
-  define('DB_USER','root');
-  define('DB_PASSWORD','');
+  define('DB_USER','customer');
+  define('DB_PASSWORD','CustomerShopPlus777');
   function connectDB(){
     $connect = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Cannot connect to database");
     return $connect;
